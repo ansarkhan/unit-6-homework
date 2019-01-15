7772353d6fcaf4522347af962b2d72324f62e8fb
ansarkhan <ansarkhan@me.com>
2019-01-15 17:13:01 -0500

    notes added, finalized layout

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # unit-6-homework
+
+To dos:
+
+// Create form in HTML
+  // Upon form submission, add value to array
+  // upon form submission, remove value from array
+
+// Array of all values
+
+// Onclick, API call to get results using value of button, clear old results
+
+// Container to display all the Gifs
+    // include rating
+    // include other variables
+
+// pause and play GIFs
+
+// clear screen button
+
+// Bonus material
+
+
--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 /*
-To Dos
+API Key: Vlb8ghCOyURWJlLKsnh82oj1tgWNDDY1
 
-// Create form in HTML
-  // Upon form submission, add value to array
+SAMPLE API CALLS W/ DESCRIPTORS
 
-// Array of all values
+#1
+Paramaters:
+endpoint = search;
+q = obama;
+limit = 25;
+offset = 5; (optional)
+rating = G;
+lang = eng;
 
-// Onclick, API call to get results using value of button
+https://api.giphy.com/v1/gifs/search?api_key=Vlb8ghCOyURWJlLKsnh82oj1tgWNDDY1&q=obama&limit=25&offset=5&rating=G&lang=en
 
-// Container to display all the Gifs
 
-// Bonus material
+#2
+Parameters:
+endpoint = translate;
+s = happy;
 
+https://api.giphy.com/v1/gifs/translate?api_key=Vlb8ghCOyURWJlLKsnh82oj1tgWNDDY1&q&s=happy
+
+#3
+Parameters
+endpoint = trending
+limit = 25
+rating = G
+
+https://api.giphy.com/v1/gifs/trending?api_key=Vlb8ghCOyURWJlLKsnh82oj1tgWNDDY1&limit=25&rating=G
+
+other endpoints: Random, Get by ID, Get by IDs
 
 */
