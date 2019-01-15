a28bde35cfab6befbb6d8e46b298a836a6050f36
ansarkhan <ansarkhan@me.com>
2019-01-15 10:36:25 -0500

    starter html and pseudocode

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+To Dos
+
+// Create form in HTML
+  // Upon form submission, add value to array
+
+// Array of all values
+
+// Onclick, API call to get results using value of button
+
+// Container to display all the Gifs
+
+// Bonus material
+
+
+*/
